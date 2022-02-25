d9f3c4cb46ef269672897e4060d0a86ceb270932
Hmdo <muhammedaahaj@gmail.com>
2022-02-25 15:40:52 +0300

    Create new file

---
+++ script.js
@@ -0,0 +1,13 @@
+var currency = 'RY';
+var data = {
+    1: { img: "1.webp", name: "White shoes with yellow", price: "8,000" },
+    2: { img: "2.jpg", name: "Gray Shoes Sport", price: "10,000" },
+    3: { img: "3.png", name: "black Shoes with white", price: "4,000" },
+    4: { img: "4.webp", name: "gray Shoes BSR", price: "6,000" },
+    5: { img: "5.jpg", name: "Sport Shoes Adidas", price: "10,000" },
+    6: { img: "6.jpg", name: "Sky Shoes with Rainbow colours", price: "7,000" },
+    7: { img: "7.jfif", name: "Shoes walker", price: "8,000" },
+    8: { img: "8.jpg", name: "gray Shoes CLE", price: "9,000" },
+    9: { img: "9.jpeg", name: "white shoes with black", price: "11,000" },
+    10: { img: "10.jpg", name: "Adidas black shoes with blue", price: "12,000" },
+}
